863f3d4210f249e12eb6907f91540ec4f4c23ef5
ryland goldstein <rylandgoldstein@rylands-MacBook-Pro.local>
2025-07-10 16:25:41 -0700

    working linting and test configuration

---
+++ vitest.config.ts
@@ -0,0 +1,39 @@
+import path from 'node:path';
+
+import { mergeConfig } from 'vitest/config';
+
+import configuration from './vite.config';
+
+export default mergeConfig(configuration, {
+  test: {
+    globals: true,
+    environment: 'happy-dom',
+    include: ['src/**/*.{test,spec}.{js,ts,tsx}'],
+    exclude: ['node_modules', 'dist', 'tests', 'coverage'],
+    testTimeout: 10000,
+    hookTimeout: 10000,
+    teardownTimeout: 10000,
+    pool: 'forks',
+    reporters: ['verbose'],
+    coverage: {
+      provider: 'v8',
+      reporter: ['text', 'json', 'html'],
+      exclude: [
+        'node_modules/',
+        'src/test/',
+        '**/*.d.ts',
+        '**/*.config.*',
+        '**/coverage/**',
+        'scripts/**',
+        'tests/**',
+      ],
+    },
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve('src'),
+      '$tools': path.resolve('src/tools/index.ts'),
+      '$styles': path.resolve('src/components/styles/index.ts'),
+    },
+  },
+});
